b19bec9fc446054d319cace07e0d934813b4ea6b
switcake <switcake@hotmail.com>
2020-03-16 15:35:44 -0700

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -1,4 +1,4 @@
 (function() {
-   document.cookie = "TEST_COOKIE=123; domain=.github.com; SameSite=None; secure";
+   document.cookie = "TEST_COOKIE=123; domain=.github.io; SameSite=None; secure";
 
 })();
